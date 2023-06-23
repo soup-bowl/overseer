0cfee30ef153fd6b20b36ddbdbd2516c03d74f30
soup-bowl <code@soupbowl.io>
2023-06-23 08:36:59 +0100

    Updog?

--- main.py
+++ main.py
@@ -3,6 +3,7 @@
 import os, sys, subprocess, requests, json
 
 from dotenv import load_dotenv
+from pysnmp.hlapi import *
 from PIL import Image, ImageFont, ImageDraw
 from font_hanken_grotesk import HankenGroteskBold, HankenGroteskMedium
 from font_intuitive import Intuitive
@@ -18,6 +19,16 @@ def format_number(num):
 	else:
 		return str(value)
 
+def is_device_online(ip_address):
+    command = ['ping', '-c', '1', ip_address]
+    try:
+        output = subprocess.check_output(command, stderr=subprocess.STDOUT, universal_newlines=True)
+        if '1 packets transmitted, 1 received' in output:
+            return True
+    except subprocess.CalledProcessError:
+        pass
+    return False
+
 def get_pihole():
 	try:
 		response = requests.get(f"http://swan.dharma:54321/admin/api.php?auth={os.getenv('PIHOLE_KEY')}&summary")
@@ -28,6 +39,16 @@ def get_pihole():
 		return None
 
 	return response.json()
+
+def get_snmp_data(host, community, oid):
+	error_indication, error_status, error_index, var_binds = next(
+		getCmd(SnmpEngine(), CommunityData(community), UdpTransportTarget((host, 161)), ContextData(), ObjectType(ObjectIdentity(oid)))
+	)
+
+	if error_indication or error_status:
+		return None
+
+	return var_binds[0][1]
 
 load_dotenv()
 
@@ -43,27 +64,43 @@ padding = -5
 img = Image.new("P", inky_display.resolution)
 draw = ImageDraw.Draw(img)
 
-hanken_bold_font = ImageFont.truetype(HankenGroteskBold, int(12 * scale_size))
+hanken_bold_font = ImageFont.truetype(HankenGroteskBold, int(10 * scale_size))
 hanken_medium_font = ImageFont.truetype(HankenGroteskMedium, int(10 * scale_size))
 
 y_top = int(inky_display.height * (5.0 / 10.0))
 y_bottom = y_top + int(inky_display.height * (4.0 / 10.0))
+x_barrier = 50
 
 # Make the background black
 for y in range(0, inky_display.height):
 	for x in range(0, inky_display.width):
 		img.putpixel((x, y), inky_display.BLACK)
 
-draw.text((2, (0 * 12)), "LP Network Information", inky_display.RED, font=hanken_medium_font)
-draw.text((2, (1 * 12)), "Pi Firewall:", inky_display.WHITE, font=hanken_medium_font)
+draw.text((2, (0 * 12)), "DHARMA NETWORK", inky_display.RED, font=hanken_bold_font)
 
+draw.text((2, (1 * 12)), "Pihole:", inky_display.WHITE, font=hanken_medium_font)
 if pidata is not None:
 	blockrate = f"Block {format_number(pidata.get('ads_blocked_today'))}/{format_number(pidata.get('dns_queries_today'))}"
 	blocknum  = f"{pidata.get('ads_percentage_today')}% block rate"
-	draw.text((72, (1 * 12)), blockrate, inky_display.WHITE, font=hanken_medium_font)
-	draw.text((72, (2 * 12)), blocknum, inky_display.WHITE, font=hanken_medium_font)
+	draw.text((x_barrier, (1 * 12)), blockrate, inky_display.WHITE, font=hanken_medium_font)
+	draw.text((x_barrier, (2 * 12)), blocknum, inky_display.WHITE, font=hanken_medium_font)
 else:
-	draw.text((72, (1 * 12)), "OFFLINE", inky_display.RED, font=hanken_medium_font)
+	draw.text((x_barrier, (1 * 12)), "OFFLINE", inky_display.RED, font=hanken_medium_font)
+
+servers = ["swan", "pearl", "orchid"]
+for y in range(0, 3):
+	pos = y + 3
+	draw.text((2, (pos * 12)), f"{servers[y]}:", inky_display.WHITE, font=hanken_medium_font)
+	if is_device_online(f"{servers[y]}.dharma"):
+		draw.text((x_barrier, (pos * 12)), "ONLINE", inky_display.WHITE, font=hanken_medium_font)
+	else:
+		draw.text((x_barrier, (pos * 12)), "OFFLINE", inky_display.RED, font=hanken_medium_font)
+
+draw.text((2, (6 * 12)), "tower:", inky_display.WHITE, font=hanken_medium_font)
+if is_device_online(os.getenv('WATCHTOWER_ADDR')):
+	draw.text((x_barrier, (6 * 12)), "ONLINE", inky_display.WHITE, font=hanken_medium_font)
+else:
+	draw.text((x_barrier, (6 * 12)), "OFFLINE", inky_display.RED, font=hanken_medium_font)
 
 inky_display.set_image(img)
 inky_display.show()
