d984567f7e14f69996d06c9f8b3978b4680236ae
soup-bowl <code@soupbowl.io>
2023-06-23 08:52:26 +0100

    bigger text

--- main.py
+++ main.py
@@ -64,43 +64,44 @@ padding = -5
 img = Image.new("P", inky_display.resolution)
 draw = ImageDraw.Draw(img)
 
-hanken_bold_font = ImageFont.truetype(HankenGroteskBold, int(10 * scale_size))
-hanken_medium_font = ImageFont.truetype(HankenGroteskMedium, int(10 * scale_size))
+hanken_bold_font = ImageFont.truetype(HankenGroteskBold, int(13 * scale_size))
+hanken_medium_font = ImageFont.truetype(HankenGroteskMedium, int(13 * scale_size))
 
 y_top = int(inky_display.height * (5.0 / 10.0))
 y_bottom = y_top + int(inky_display.height * (4.0 / 10.0))
 x_barrier = 50
+y_spacer = 13
 
 # Make the background black
 for y in range(0, inky_display.height):
 	for x in range(0, inky_display.width):
 		img.putpixel((x, y), inky_display.BLACK)
 
-draw.text((2, (0 * 12)), "DHARMA NETWORK", inky_display.RED, font=hanken_bold_font)
+draw.text((2, (0 * y_spacer)), "DHARMA NETWORK", inky_display.RED, font=hanken_bold_font)
 
-draw.text((2, (1 * 12)), "Pihole:", inky_display.WHITE, font=hanken_medium_font)
+draw.text((2, (1 * y_spacer)), "Pihole:", inky_display.WHITE, font=hanken_medium_font)
 if pidata is not None:
 	blockrate = f"Block {format_number(pidata.get('ads_blocked_today'))}/{format_number(pidata.get('dns_queries_today'))}"
 	blocknum  = f"{pidata.get('ads_percentage_today')}% block rate"
-	draw.text((x_barrier, (1 * 12)), blockrate, inky_display.WHITE, font=hanken_medium_font)
-	draw.text((x_barrier, (2 * 12)), blocknum, inky_display.WHITE, font=hanken_medium_font)
+	draw.text((x_barrier, (1 * y_spacer)), blockrate, inky_display.WHITE, font=hanken_medium_font)
+	draw.text((x_barrier, (2 * y_spacer)), blocknum, inky_display.WHITE, font=hanken_medium_font)
 else:
-	draw.text((x_barrier, (1 * 12)), "OFFLINE", inky_display.RED, font=hanken_medium_font)
+	draw.text((x_barrier, (1 * y_spacer)), "OFFLINE", inky_display.RED, font=hanken_medium_font)
 
 servers = ["swan", "pearl", "orchid"]
 for y in range(0, 3):
 	pos = y + 3
-	draw.text((2, (pos * 12)), f"{servers[y]}:", inky_display.WHITE, font=hanken_medium_font)
+	draw.text((2, (pos * y_spacer)), f"{servers[y]}:", inky_display.WHITE, font=hanken_medium_font)
 	if is_device_online(f"{servers[y]}.dharma"):
-		draw.text((x_barrier, (pos * 12)), "ONLINE", inky_display.WHITE, font=hanken_medium_font)
+		draw.text((x_barrier, (pos * y_spacer)), "ONLINE", inky_display.WHITE, font=hanken_medium_font)
 	else:
-		draw.text((x_barrier, (pos * 12)), "OFFLINE", inky_display.RED, font=hanken_medium_font)
+		draw.text((x_barrier, (pos * y_spacer)), "OFFLINE", inky_display.RED, font=hanken_medium_font)
 
-draw.text((2, (6 * 12)), "tower:", inky_display.WHITE, font=hanken_medium_font)
+draw.text((2, (6 * y_spacer)), "tower:", inky_display.WHITE, font=hanken_medium_font)
 if is_device_online(os.getenv('WATCHTOWER_ADDR')):
-	draw.text((x_barrier, (6 * 12)), "ONLINE", inky_display.WHITE, font=hanken_medium_font)
+	draw.text((x_barrier, (6 * y_spacer)), "ONLINE", inky_display.WHITE, font=hanken_medium_font)
 else:
-	draw.text((x_barrier, (6 * 12)), "OFFLINE", inky_display.RED, font=hanken_medium_font)
+	draw.text((x_barrier, (6 * y_spacer)), "OFFLINE", inky_display.RED, font=hanken_medium_font)
 
 inky_display.set_image(img)
 inky_display.show()
