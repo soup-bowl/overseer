e5797bfdeeb70eeebc8569788ceddab4e0e02904
soup-bowl <code@soupbowl.io>
2023-07-02 15:20:09 +0100

    Basic SSH command support.

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-import subprocess, requests, yaml, time
+import subprocess, requests, yaml, time, paramiko
 
 from pysnmp.hlapi import *
 from PIL import Image, ImageFont, ImageDraw
@@ -60,6 +60,21 @@ def get_linode(instance, token):
 		return None
 
 	return response.json()
+
+def get_ssh(server, user, key, command):
+	client = paramiko.SSHClient()
+	client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+
+	try:
+		private_key = paramiko.RSAKey.from_private_key_file(key)
+		client.connect(server, username=user, pkey=private_key)
+		stdin, stdout, stderr = client.exec_command(command)
+		return stdout.read().decode()
+	except:
+		return None
+	finally:
+		client.close()
+
 config = read_config()
 
 inky_display = auto(ask_user=True)
@@ -88,6 +103,22 @@ draw.text((2, 0), config['name'], inky_display.RED, font=hanken_bold_font)
 
 y_placement = 1
 for stage in config['stages']:
+	if stage['type'] == 'ssh':
+		data = get_ssh(stage['address'], stage['user'], stage['auth'], stage['command'])
+		draw.text((2, (y_placement * y_spacer)), f"{stage['label']}:", inky_display.WHITE, font=hanken_medium_font)
+		if data is not None:
+			text   = data.replace("\n", "")
+			prefix = stage['prefix'] if 'prefix' in stage else ''
+			suffix = stage['suffix'] if 'suffix' in stage else ''
+			draw.text(
+				(x_barrier, (y_placement * y_spacer)),
+				f"{prefix}{text}{suffix}",
+				inky_display.WHITE,
+				font=hanken_medium_font
+			)
+		else:
+			draw.text((x_barrier, (y_placement * y_spacer)), "ERROR", inky_display.RED, font=hanken_medium_font)
+		y_placement = y_placement + 1
 	if stage['type'] == 'pihole':
 		pidata = get_pihole(stage['address'], stage['auth'])
 		draw.text((2, (y_placement * y_spacer)), f"{stage['label']}:", inky_display.WHITE, font=hanken_medium_font)
