7d813b7c7c4205087389a138bf225193faa0f3e0
soup-bowl <code@soupbowl.io>
2023-09-17 22:50:00 +0100

    Moved stage processing to func

--- main.py
+++ main.py
@@ -25,6 +25,46 @@ button_a = Button(12)
 button_b = Button(13)
 button_c = Button(14)
 
+def display_info(conf):
+    for stage in conf:
+        if stage['type'] == "pihole":
+            pihole = Remote.get_pihole(stage['address'], stage['auth'])
+            if pihole is not None:
+                data = Process.display_block_data(pihole)
+                pico_screen.write_line(stage['label'], data)
+            else:
+                pico_screen.write_line(stage['label'], "OFFLINE", 10)
+        elif stage['type'] == "synology":
+            synnas = Remote.get_synology_nas(stage['address'], stage['user'], stage['auth'])
+            if synnas is not None and 'data' in synnas:
+                pico_screen.write_line(stage['label'], Process.display_nas_data(synnas['data']['volumes']))
+            else:
+                pico_screen.write_line(stage['label'], "OFFLINE", 10)
+        elif stage['type'] == "prometheus":
+            job = stage['user']
+            try:
+                cpu_aloc = Remote.get_prometheus(stage['address'], "100%20-%20(avg%20by%20(mode)%20(irate(node_cpu_seconds_total%7Bmode%3D%22idle%22%2Cjob%3D%22" + job + "%22%7D%5B5m%5D))%20*%20100)%0A")
+                ram_aloc = Remote.get_prometheus(stage['address'], "100%20-%20((node_memory_MemAvailable_bytes%7Bjob%3D%22" + job + "%22%7D%20%2F%20node_memory_MemTotal_bytes%7Bjob%3D%22" + job + "%22%7D)%20*%20100)%0A")
+                cpu_temp = Remote.get_prometheus(stage['address'], "avg(node_hwmon_temp_celsius%7Bchip%3D%22platform_coretemp_0%22%2Cjob%3D%22" + job + "%22%2Csensor%3D~%22temp%5B1-5%5D%22%7D)")
+            except:
+                cpu_aloc = None
+                ram_aloc = None
+                cpu_temp = None
+            if cpu_aloc is not None and cpu_temp is not None and ram_aloc is not None:
+                if ('data' in cpu_aloc and 'result' in cpu_aloc['data'] and isinstance(cpu_aloc['data']['result'], list) and len(cpu_aloc['data']['result']) > 0
+                and 'data' in cpu_temp and 'result' in cpu_temp['data'] and isinstance(cpu_temp['data']['result'], list) and len(cpu_temp['data']['result']) > 0
+                and 'data' in ram_aloc and 'result' in ram_aloc['data'] and isinstance(ram_aloc['data']['result'], list) and len(ram_aloc['data']['result']) > 0):
+                    pico_screen.write_line(stage['label'], Process.display_prometheus_details(
+                        cpu_aloc['data']['result'],
+                        ram_aloc['data']['result'],
+                        cpu_temp['data']['result'])
+                    )
+                else:
+                    pico_screen.write_line(stage['label'], "NO DATA", 10)
+            else:
+                pico_screen.write_line(stage['label'], "OFFLINE", 10)
+
+
 # set up
 pico_screen.quick_text("Ready")
 time.sleep(0.5)
@@ -82,45 +122,7 @@ while True:
             if abs(start_time - utime.time()) > 300:
                 pico_screen.inform_loading()
                 pico_screen.clear(True)
-                
-                for stage in conf['stages']:
-                    if stage['type'] == "pihole":
-                        pihole = Remote.get_pihole(stage['address'], stage['auth'])
-                        if pihole is not None:
-                            data = Process.display_block_data(pihole)
-                            pico_screen.write_line(stage['label'], data)
-                        else:
-                            pico_screen.write_line(stage['label'], "OFFLINE", 10)
-                    elif stage['type'] == "synology":
-                        synnas = Remote.get_synology_nas(stage['address'], stage['user'], stage['auth'])
-                        if synnas is not None and 'data' in synnas:
-                            pico_screen.write_line(stage['label'], Process.display_nas_data(synnas['data']['volumes']))
-                        else:
-                            pico_screen.write_line(stage['label'], "OFFLINE", 10)
-                    elif stage['type'] == "prometheus":
-                        job = stage['user']
-                        try:
-                            cpu_aloc = Remote.get_prometheus(stage['address'], "100%20-%20(avg%20by%20(mode)%20(irate(node_cpu_seconds_total%7Bmode%3D%22idle%22%2Cjob%3D%22" + job + "%22%7D%5B5m%5D))%20*%20100)%0A")
-                            ram_aloc = Remote.get_prometheus(stage['address'], "100%20-%20((node_memory_MemAvailable_bytes%7Bjob%3D%22" + job + "%22%7D%20%2F%20node_memory_MemTotal_bytes%7Bjob%3D%22" + job + "%22%7D)%20*%20100)%0A")
-                            cpu_temp = Remote.get_prometheus(stage['address'], "avg(node_hwmon_temp_celsius%7Bchip%3D%22platform_coretemp_0%22%2Cjob%3D%22" + job + "%22%2Csensor%3D~%22temp%5B1-5%5D%22%7D)")
-                        except:
-                            cpu_aloc = None
-                            ram_aloc = None
-                            cpu_temp = None
-                        if cpu_aloc is not None and cpu_temp is not None and ram_aloc is not None:
-                            if ('data' in cpu_aloc and 'result' in cpu_aloc['data'] and isinstance(cpu_aloc['data']['result'], list) and len(cpu_aloc['data']['result']) > 0
-                            and 'data' in cpu_temp and 'result' in cpu_temp['data'] and isinstance(cpu_temp['data']['result'], list) and len(cpu_temp['data']['result']) > 0
-                            and 'data' in ram_aloc and 'result' in ram_aloc['data'] and isinstance(ram_aloc['data']['result'], list) and len(ram_aloc['data']['result']) > 0):
-                                pico_screen.write_line(stage['label'], Process.display_prometheus_details(
-                                    cpu_aloc['data']['result'],
-                                    ram_aloc['data']['result'],
-                                    cpu_temp['data']['result'])
-                                )
-                            else:
-                                pico_screen.write_line(stage['label'], "NO DATA", 10)
-                        else:
-                            pico_screen.write_line(stage['label'], "OFFLINE", 10)
-
+                display_info(conf['stages'])
                 pico_screen.commit()
 
                 start_time = utime.time()
